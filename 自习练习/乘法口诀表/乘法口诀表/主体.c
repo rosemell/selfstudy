fcb144f5ddeee0c2477829108b0c404cf53d78d5
rosemell <109337532+rosemell@users.noreply.github.com>
2022-08-03 15:53:31 +0800

    Add files via upload

---
+++ 自习练习/乘法口诀表/乘法口诀表/主体.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//vs可将上述代码复制到文件newc++file.cpp,以保证新建自带
+#include <stdio.h>
+int main()
+{
+	int a = 0, b = 0, i = 0;
+	while (a < 9)
+	{
+		while (b < 9)
+		{
+			printf("%d*%d=%d   ", a, b,a*b);
+			b++;
+		}
+		printf("\n");
+		a++;
+		b = 0;
+	}
+	
+	return 0;
+}
---
+++ 自习练习/乘法口诀表/乘法口诀表/源.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//vs可将上述代码复制到文件newc++file.cpp,以保证新建自带
+#include <stdio.h>
+//不同数据类型大小的输出（单位：字节）
+//int main()
+//{
+//	printf("%d",sizeof(short));
+//	printf("%d", sizeof(long));
+//	printf("%d", sizeof(long long));
+//	printf("%d", sizeof(int));
+//	printf("%d", sizeof(double));
+//  printf("%d", sizeof(char));
+//	return 0;
+//	}
+//int main()
+//{
+//	int a = 0;
+//	int b;
+//	scanf("%d %d", &b,&a);
+//	printf("b=%d\n", b);
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = { 'a','b'};
+//	char arr[] = "ab";
+//	return 0;
+//}
+//int main()
+//{
+//	int a;
+//	int b;
+//	int sum;
+//	printf("输入a和b\n");
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("a+b=%d", sum);
+//	return 0;
+//}
